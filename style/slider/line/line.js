b0b97a3b4af0cf3f3f0b0c506a558b0a18db9843
PhanHoangSon-28-05 <hoangson28052002@gmail.com>
2024-07-29 07:24:41 +0700

    Update 7/29

--- style/slider/line/line.js
+++ style/slider/line/line.js
@@ -1,21 +1,21 @@
 
-    var currentSlide = 0;
-    var slides = document.querySelectorAll('#slideshowLine .item');
+var currentSlide = 0;
+var slides = document.querySelectorAll('#slideshowLine .item');
 
-    function showSlideLine(index) {
-        slides[currentSlide].classList.remove('active');
-        slides[index].classList.add('active');
-        currentSlide = index;
-    }
+function showSlideLine(index) {
+    slides[currentSlide].classList.remove('active');
+    slides[index].classList.add('active');
+    currentSlide = index;
+}
 
-    function nextSlideLine() {
-        var nextIndex = (currentSlide + 1) % slides.length;
-        showSlideLine(nextIndex);
-    }
+function nextSlideLine() {
+    var nextIndex = (currentSlide + 1) % slides.length;
+    showSlideLine(nextIndex);
+}
 
-    function prevSlideLine() {
-        var prevIndex = (currentSlide - 1 + slides.length) % slides.length;
-        showSlideLine(prevIndex);
-    }
+function prevSlideLine() {
+    var prevIndex = (currentSlide - 1 + slides.length) % slides.length;
+    showSlideLine(prevIndex);
+}
 
-    setInterval(nextSlideLine, 2000);
+setInterval(nextSlideLine, 2000);
